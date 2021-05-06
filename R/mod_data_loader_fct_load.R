f4757f3b6de9379f0e094a153b3592413825eae0
Adrienne Stilp <amstilp@uw.edu>
2021-05-06 15:44:54 -0700

    Add skeleton function definitions for data_loader

---
+++ R/mod_data_loader_fct_load.R
@@ -0,0 +1,11 @@
+.load_null_model_file <- function(filename) {}
+
+.load_phenotype_file <- function(filename) {}
+
+.load_data <- function(null_model_filename, phenotype_filename) {}
+
+.check_null_model <- function(null_model) {}
+
+.check_phenotype <- function (phen) {}
+
+.check_data <- function(null_model, phen) {}
