92f3fd13cb539c04155cd8f915bb6939e062a7c9
Adrienne Stilp <amstilp@uw.edu>
2021-05-06 15:12:53 -0700

    Use rlang to fix check warning

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -15,6 +15,7 @@
 
 ## Dependencies ----
 ## Add one line by package you want to add as dependency
+usethis::use_package( "rlang" )
 usethis::use_package( "dplyr" )
 usethis::use_package( "ggplot2" )
 
