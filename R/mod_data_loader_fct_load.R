82ead4e0c7736e9e59c764921f8e0e2e2f6652bb
Adrienne Stilp <amstilp@uw.edu>
2021-05-13 16:53:25 -0700

    Add function to load a null model

--- R/mod_data_loader_fct_load.R
+++ R/mod_data_loader_fct_load.R
@@ -1,4 +1,23 @@
-.load_null_model_file <- function(filename) {}
+.load_null_model <- function(filename) {
+  tmp <- get(load(filename))
+
+  # Check that it's a valid nuull model file.
+  allowed_classes <- c("GENESIS.nullMixedModel", "GENESIS.nullModel")
+  cls <- class(tmp)
+  if (!(class(tmp) %in% allowed_classes)) {
+    err <- sprintf("%s is not a valid GENESIS null model.", filename)
+    stop(err)
+  }
+  # Some users may be using older null model formatss.
+  tmp <- GENESIS:::.updateNullModelFormat(tmp)
+
+  null_model <- tmp$fit %>%
+    # Change names to have prefix "Model: "
+    dplyr::rename_with(.fn = ~ paste0("Model: ", .x), -sample.id)
+  # No rownames.
+  rownames(null_model) <- NULL
+  null_model
+}
 
 .load_phenotype_file <- function(filename) {}
 
--- tests/testthat/test-mod-data-loader.R
+++ tests/testthat/test-mod-data-loader.R
@@ -1,2 +1,24 @@
-test_that("add tests", {
+test_that("load null model works as expected", {
+  nullmod_file <- system.file("extdata", "null_model.RData", package="shinyNullModel")
+  nm <- get(load(nullmod_file))
+  out <- .load_null_model(nullmod_file)
+  expect_equal(out$fit$sample.id, nm$sample.id)
+  vars <- setdiff(names(out), "sample.id")
+  expect_true(all(stringr::str_starts(vars, "Model: "))) # Appends model: to the names
+})
+
+test_that("load null model data updates old null models", {
+  nullmod_file <- system.file("extdata", "null_model_old.RData", package="shinyNullModel")
+  nm <- get(load(nullmod_file))
+  expect_warning(out <- .load_null_model(nullmod_file), "updated")
+  expect_equal(out$sample.id, nm$sample.id)
+  vars <- setdiff(names(out), "sample.id")
+  expect_true(all(stringr::str_starts(vars, "Model: "))) # Appends model: to the names
+})
+
+test_that("load null model fails iwth incorrect data type", {
+  nm <- c(1, 2, 3)
+  tmpfile <- tempfile()
+  save(nm, file = tmpfile)
+  expect_error(.load_null_model(tmpfile), "valid GENESIS null model")
 })
