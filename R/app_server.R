7be392a39d3a808fc53228e60a7dc07870fcbbc9
Adrienne Stilp <amstilp@uw.edu>
2021-05-25 11:07:06 -0700

    Use loaded dataset names for x and y axis select boxes

--- R/app_server.R
+++ R/app_server.R
@@ -10,6 +10,6 @@ app_server <- function( input, output, session ) {
 
   # Your application server logic
   mod_data_loader_server("data_loader_ui_1", r)
-  mod_var_selector_server("var_selector_ui_1")
+  mod_var_selector_server("var_selector_ui_1", r)
   mod_plot_server("plot_ui_1")
 }
--- R/mod_var_selector.R
+++ R/mod_var_selector.R
@@ -19,9 +19,22 @@ mod_var_selector_ui <- function(id){
 #' var_selector Server Functions
 #'
 #' @noRd
-mod_var_selector_server <- function(id){
+mod_var_selector_server <- function(id, r){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+
+    r$var_selector <- reactiveValues(
+      x = NULL,
+      y = NULL
+    )
+    # Update x and y axis selections based on loaded data.
+    observe({
+      updateSelectInput(session, "x", choices = .get_variable_names(r$data_loader$dataset))
+    })
+
+    observe({
+      updateSelectInput(session, "y", choices = .get_variable_names(r$data_loader$dataset))
+    })
 
   })
 }
