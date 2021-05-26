0150ca0678116bc8aefa528444c11d257e9cb3e9
Adrienne Stilp <amstilp@uw.edu>
2021-05-26 15:53:02 -0700

    Update plotting helper function to plot just one variable
    If only an x variable is specified, but no y variable, plot a
    histogram or barplot.

--- R/mod_plot_fct_plot.R
+++ R/mod_plot_fct_plot.R
@@ -6,27 +6,38 @@
 #' @importFrom ggplot2 geom_point
 #' @importFrom ggplot2 geom_boxplot
 #' @importFrom ggplot2 coord_flip
-.generate_plot <- function(dat, x_var, y_var, group = NULL) {
+.generate_plot <- function(dat, x_var, y_var = NULL, group = NULL) {
   # This is using functions in the var_selector module. TODO: improve this?
   type_x <- .detect_variable_type(dat[[x_var]])
-  type_y <- .detect_variable_type(dat[[y_var]])
 
-  p <- ggplot(dat, aes_string(x = as.name(x_var), y = as.name(y_var)))
-  if (type_x == QUANTITATIVE & type_y == QUANTITATIVE) {
-    # Show a scatterplot.
-    p <- p + geom_point(aes_string(color = group))
-  } else if (type_x == QUANTITATIVE & type_y == CATEGORICAL) {
-    # Show a flipped boxplot.
-    # We have to recreate p because we need to use coord_flip.
-    p <- ggplot(dat, aes_string(y = as.name(x_var), x = as.name(y_var))) +
-      geom_boxplot(aes_string(fill = group)) +
-      coord_flip()
-  } else if (type_x == CATEGORICAL & type_y == QUANTITATIVE) {
-    # Show a boxplot.
-    p <- p + geom_boxplot(aes_string(fill = group))
-  } else if (type_y == CATEGORICAL & type_y == CATEGORICAL) {
-    # Maybe we don't want to allow this?
-    stop("Cannot plot two categorical variables against each other.")
+  if (is.null(y_var)) {
+    p <- ggplot(dat, aes_string(x = as.name(x_var)))
+    if (type_x == QUANTITATIVE) {
+      p <- p + geom_histogram(aes_string(fill = group))
+    } else if (type_x == CATEGORICAL) {
+      p <- p + geom_bar(aes_string(fill = group))
+    }
+  }
+  else {
+    type_y <- .detect_variable_type(dat[[y_var]])
+
+    p <- ggplot(dat, aes_string(x = as.name(x_var), y = as.name(y_var)))
+    if (type_x == QUANTITATIVE & type_y == QUANTITATIVE) {
+      # Show a scatterplot.
+      p <- p + geom_point(aes_string(color = group))
+    } else if (type_x == QUANTITATIVE & type_y == CATEGORICAL) {
+      # Show a flipped boxplot.
+      # We have to recreate p because we need to use coord_flip.
+      p <- ggplot(dat, aes_string(y = as.name(x_var), x = as.name(y_var))) +
+        geom_boxplot(aes_string(fill = group)) +
+        coord_flip()
+    } else if (type_x == CATEGORICAL & type_y == QUANTITATIVE) {
+      # Show a boxplot.
+      p <- p + geom_boxplot(aes_string(fill = group))
+    } else if (type_y == CATEGORICAL & type_y == CATEGORICAL) {
+      # Maybe we don't want to allow this?
+      stop("Cannot plot two categorical variables against each other.")
+    }
   }
   return(p)
 }
--- tests/testthat/test-mod-plot-fct.R
+++ tests/testthat/test-mod-plot-fct.R
@@ -1,6 +1,40 @@
 context("test-mod-plot-fct") #  - required for vdiffr
 library(vdiffr)
 
+test_that("generate plot with xvar only",{
+  set.seed(123)
+  n <- 100
+  dat <- data.frame(
+    a = rnorm(n),
+    b = rnorm(n),
+    c = sample(letters[1:3], n, replace = T),
+    d = sample(letters[1:3], n, replace = T)
+  )
+
+  # histogram
+  expect_doppelganger("x histogram", .generate_plot(dat, "a"))
+  # bar plot
+  expect_doppelganger("x barplot", .generate_plot(dat, "c"))
+})
+
+test_that("generate plot with xvar and group",{
+  set.seed(123)
+  n <- 100
+  dat <- data.frame(
+    a = rnorm(n),
+    b = rnorm(n),
+    c = sample(letters[1:3], n, replace = T),
+    d = sample(letters[1:3], n, replace = T)
+  )
+
+  # histogram
+  expect_doppelganger("x histogram grouped", .generate_plot(dat, "a", group = "c"))
+  # bar plot
+  expect_doppelganger("x barplot grouped", .generate_plot(dat, "c", group = "d"))
+  # not allowed - grouping by a quantitative variable.
+})
+
+
 test_that("generate plot with xvar and yvar only", {
   # fake data
   set.seed(123)
@@ -12,11 +46,11 @@ test_that("generate plot with xvar and yvar only", {
     d = sample(letters[1:3], n, replace = T)
   )
   # scatterplot
-  expect_doppelganger("scatterplot", .generate_plot(dat, "a", "b"))
+  expect_doppelganger("xy scatterplot", .generate_plot(dat, "a", "b"))
   # boxplot
-  expect_doppelganger("boxplot", .generate_plot(dat, "c", "a"))
+  expect_doppelganger("xy boxplot", .generate_plot(dat, "c", "a"))
   # flipped boxplot
-  expect_doppelganger("flipped boxplot", .generate_plot(dat, "a", "c"))
+  expect_doppelganger("xy flipped boxplot", .generate_plot(dat, "a", "c"))
   # error
   expect_error(.generate_plot(dat, "c", "d"), "two categorical variables")
 })
@@ -30,14 +64,16 @@ test_that("generate plot with group specified", {
     b = rnorm(n),
     c = sample(letters[1:3], n, replace = T),
     d = sample(letters[1:3], n, replace = T),
-    group = sample(letters[1:3], n, replace = T)
+    group = sample(letters[1:3], n, replace = T),
+    e = rnorm(n)
   )
   # scatterplot
-  expect_doppelganger("scatterplot grouped", .generate_plot(dat, "a", "b", group = "group"))
+  expect_doppelganger("xy scatterplot grouped", .generate_plot(dat, "a", "b", group = "group"))
   # boxplot
-  expect_doppelganger("boxplot grouped", .generate_plot(dat, "c", "a", group = "group"))
+  expect_doppelganger("xy boxplot grouped", .generate_plot(dat, "c", "a", group = "group"))
   # flipped boxplot
-  expect_doppelganger("flipped boxplot grouped", .generate_plot(dat, "a", "c", group = "group"))
-  # error
+  expect_doppelganger("xy flipped boxplot grouped", .generate_plot(dat, "a", "c", group = "group"))
+  # errors
   expect_error(.generate_plot(dat, "c", "d", group = "group"), "two categorical variables")
+
 })
