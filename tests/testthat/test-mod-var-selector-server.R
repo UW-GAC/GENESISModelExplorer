e67213f28769f527f759d97447f36cfb49374e68
Adrienne Stilp <amstilp@uw.edu>
2021-06-02 16:43:17 -0700

    Remove commented out test

--- tests/testthat/test-mod-var-selector-server.R
+++ tests/testthat/test-mod-var-selector-server.R
@@ -1,18 +1,3 @@
-# test_that("x variable names update", {
-#   n <- 100
-#   dat <- tibble::tibble(
-#     a = rnorm(n),
-#     b = rnorm(n),
-#     c = sample(letters[1:3], n, replace = T),
-#     d = sample(letters[1:3], n, replace = T)
-#   )
-#   argr <- list(r = list(data_loader = list(dataset = dat)))
-#   testServer(mod_var_selector_server, args = argr, {
-#     skip("figure out how to write test")
-#   })
-#
-# })
-
 test_that("return values", {
   n <- 100
   dat <- reactiveVal(tibble::tibble(
