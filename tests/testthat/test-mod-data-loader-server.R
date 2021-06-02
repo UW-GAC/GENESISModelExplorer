0a9d6e04d28958ce44279bba14da3d4384c0cb5c
Adrienne Stilp <amstilp@uw.edu>
2021-06-01 17:18:54 -0700

    Remove unnecessary lines from data loader server tests

--- tests/testthat/test-mod-data-loader-server.R
+++ tests/testthat/test-mod-data-loader-server.R
@@ -1,10 +1,3 @@
-### TESTSERVER tests
-# testServer(mod_data_loader_server, {
-#   expect_true(TRUE)
-#   browser()
-#   session$setInputs(use_example_data = TRUE)
-# })
-
 ### SHINYDRIVER TESTS
 # These may need to use ShinyDriver because we require a button click.
 app <- shinytest::ShinyDriver$new(path="../../dev/run_dev.R")
@@ -84,60 +77,3 @@ app$stop()
 test_that("return value?", {
   skip("add test?")
 })
-
-# test_that("fails to load user data when only one file is specified", {
-# })
-
-### OLD TESTS
-# test_that(".get_data_reactive loads example data", {
-#   expected <- .load_data(
-#     system.file("extdata", "null_model.RData", package="shinyNullModel"),
-#     get(load(system.file("extdata", "phenotype.RData", package="shinyNullModel")))
-#   )
-#   input <- list(use_example_data = TRUE)
-#   out <- .get_data_reactive(input)
-#   expect_equal(out, expected)
-# })
-#
-# test_that(".get_data_reactive loads user data", {
-#
-#   null_model <- getobj(system.file("extdata", "null_model.RData", package="shinyNullModel"))
-#   # Hacky way to make a different null model file
-#   null_model$fit <- null_model$fit[1:10, ]
-#   nm_file <- withr::local_file()
-#   save(nm, file = nm_file)
-#
-#   phen <- get(load(system.file("extdata", "phenotype.RData", package="shinyNullModel")))
-#   phen <- phen[1:10, ]
-#   phen_file <- withr::local_file()
-#   save(phen, file = phen_file)
-#
-#   input <- list(
-#
-#   )
-# })
-#
-# test_that(".get_data_reactive loads example data before user data", {})
-#
-# test_that("does something", {
-#   testServer(mod_data_loader_server, {
-#     print("start")
-#     session$setInputs(use_example_data = TRUE)
-#     print(head(data_reactive()))
-#     expect_equal(1, 1)
-#     print('done')
-# #    print(head(data_reactive()))
-#   })
-# })
-# test_that("example data is loaded if requested", {
-#     expected <- .load_data(
-#       system.file("extdata", "null_model.RData", package="shinyNullModel"),
-#       system.file("extdata", "phenotype.RData", package="shinyNullModel")
-#     )
-#   shiny::testServer(server, {
-#     session$setInputs(use_example_data = TRUE)
-#     expect_equal(nrow(data_reactive()), 1)#, expected)
-#   })
-# })
-
-app$stop()
