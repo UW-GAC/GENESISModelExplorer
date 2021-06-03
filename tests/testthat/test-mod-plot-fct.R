4a4aee0ad605baba4dd53fd18236ba20aa51ee8d
Adrienne Stilp <amstilp@uw.edu>
2021-06-03 15:27:47 -0700

    Rename testdata columns for better clarity
    eg "quant1" is more meaningful than "a" - it's a quantitative variable.

--- tests/testthat/test-mod-plot-fct.R
+++ tests/testthat/test-mod-plot-fct.R
@@ -5,12 +5,12 @@ library(vdiffr)
 set.seed(123)
 n <- 100
 dat <- data.frame(
-  a = rnorm(n),
-  b = rnorm(n),
-  c = sample(letters[1:3], n, replace = T),
-  d = sample(letters[1:3], n, replace = T),
+  quant1 = rnorm(n),
+  quant2 = rnorm(n),
+  cat1 = sample(letters[1:3], n, replace = T),
+  cat2 = sample(letters[1:3], n, replace = T),
   group = sample(letters[1:3], n, replace = T),
-  e = rnorm(n)
+  quant3 = rnorm(n)
 )
 
 test_that(".check_truthiness", {
@@ -23,50 +23,50 @@ test_that(".check_truthiness", {
 
 test_that("generate plot with xvar only",{
   # histogram
-  expect_doppelganger("x histogram", .generate_plot(dat, "a"))
+  expect_doppelganger("x histogram", .generate_plot(dat, "quant1"))
   # bar plot
-  expect_doppelganger("x barplot", .generate_plot(dat, "c"))
+  expect_doppelganger("x barplot", .generate_plot(dat, "cat1"))
 })
 
 test_that("generate plot with xvar and group",{
   # histogram
-  expect_doppelganger("x histogram grouped", .generate_plot(dat, "a", group_var = "c"))
+  expect_doppelganger("x histogram grouped", .generate_plot(dat, "quant1", group_var = "cat1"))
   # bar plot
-  expect_doppelganger("x barplot grouped", .generate_plot(dat, "c", group_var = "d"))
+  expect_doppelganger("x barplot grouped", .generate_plot(dat, "cat1", group_var = "cat2"))
   # not allowed - grouping by a quantitative variable.
-  expect_error(.generate_plot(dat, "a", group_var = "a"), "Cannot group")
-  expect_error(.generate_plot(dat, "a", group_var = "b"), "Cannot group")
-  expect_error(.generate_plot(dat, "c", group_var = "a"), "Cannot group")
+  expect_error(.generate_plot(dat, "quant1", group_var = "quant1"), "Cannot group")
+  expect_error(.generate_plot(dat, "quant1", group_var = "quant2"), "Cannot group")
+  expect_error(.generate_plot(dat, "cat1", group_var = "quant1"), "Cannot group")
 })
 
 
 test_that("generate plot with xvar and yvar only", {
   # scatterplot
-  expect_doppelganger("xy scatterplot", .generate_plot(dat, "a", "b"))
+  expect_doppelganger("xy scatterplot", .generate_plot(dat, "quant1", "quant2"))
   # boxplot
-  expect_doppelganger("xy boxplot", .generate_plot(dat, "c", "a"))
+  expect_doppelganger("xy boxplot", .generate_plot(dat, "cat1", "quant1"))
   # flipped boxplot
-  expect_doppelganger("xy flipped boxplot", .generate_plot(dat, "a", "c"))
+  expect_doppelganger("xy flipped boxplot", .generate_plot(dat, "quant1", "cat1"))
   # error
-  expect_error(.generate_plot(dat, "c", "d"), "two categorical variables")
+  expect_error(.generate_plot(dat, "cat1", "cat2"), "two categorical variables")
 })
 
 test_that("generate plot with group specified", {
   # scatterplot
-  expect_doppelganger("xy scatterplot grouped", .generate_plot(dat, "a", "b", group_var = "group"))
+  expect_doppelganger("xy scatterplot grouped", .generate_plot(dat, "quant1", "quant2", group_var = "group"))
   # boxplot
-  expect_doppelganger("xy boxplot grouped", .generate_plot(dat, "c", "a", group_var = "group"))
+  expect_doppelganger("xy boxplot grouped", .generate_plot(dat, "cat1", "quant1", group_var = "group"))
   # flipped boxplot
-  expect_doppelganger("xy flipped boxplot grouped", .generate_plot(dat, "a", "c", group_var = "group"))
+  expect_doppelganger("xy flipped boxplot grouped", .generate_plot(dat, "quant1", "cat1", group_var = "group"))
   # errors
-  expect_error(.generate_plot(dat, "c", "d", group_var = "group"), "two categorical variables")
-  expect_error(.generate_plot(dat, "a", "b", group_var = "a"), "Cannot group")
-  expect_error(.generate_plot(dat, "a", "b", group_var = "b"), "Cannot group")
-  expect_error(.generate_plot(dat, "a", "b", group_var = "e"), "Cannot group")
-  expect_error(.generate_plot(dat, "c", "a", group_var = "a"), "Cannot group")
-  expect_error(.generate_plot(dat, "c", "a", group_var = "b"), "Cannot group")
-  expect_error(.generate_plot(dat, "a", "c", group_var = "a"), "Cannot group")
-  expect_error(.generate_plot(dat, "a", "c", group_var = "b"), "Cannot group")
+  expect_error(.generate_plot(dat, "cat1", "cat2", group_var = "group"), "two categorical variables")
+  expect_error(.generate_plot(dat, "quant1", "quant2", group_var = "quant1"), "Cannot group")
+  expect_error(.generate_plot(dat, "quant1", "quant2", group_var = "quant2"), "Cannot group")
+  expect_error(.generate_plot(dat, "quant1", "quant2", group_var = "quant3"), "Cannot group")
+  expect_error(.generate_plot(dat, "cat1", "quant1", group_var = "quant1"), "Cannot group")
+  expect_error(.generate_plot(dat, "cat1", "quant1", group_var = "quant2"), "Cannot group")
+  expect_error(.generate_plot(dat, "quant1", "cat1", group_var = "quant1"), "Cannot group")
+  expect_error(.generate_plot(dat, "quant1", "cat1", group_var = "quant2"), "Cannot group")
 
 })
 
@@ -75,16 +75,16 @@ test_that("variable names with spaces", {
   names(tmp_dat) <- paste("var", names(tmp_dat))
   # No errors:
   # no y variable
-  .generate_plot(tmp_dat, "var a") # histogram
-  .generate_plot(tmp_dat, "var c") # bar plot
-  .generate_plot(tmp_dat, "a", group_var = "c") # grouped histogram
-  .generate_plot(tmp_dat, "c", group_var = "d") # grouped bar plot
+  .generate_plot(tmp_dat, "var quant1") # histogram
+  .generate_plot(tmp_dat, "var cat1") # bar plot
+  .generate_plot(tmp_dat, "var quant1", group_var = "var cat1") # grouped histogram
+  .generate_plot(tmp_dat, "var cat1", group_var = "var cat2") # grouped bar plot
   # with y variable
-  .generate_plot(tmp_dat, "var a", "var b") # scatterplot
-  .generate_plot(tmp_dat, "var c", "var a") # boxplot
-  .generate_plot(tmp_dat, "var a", "var c") # flipped boxplot
-  .generate_plot(tmp_dat, "var a", "var b", group_var = "var group") # grouped scatterplot
-  .generate_plot(tmp_dat, "var c", "var a", group_var = "var group") # grouped boxplot
-  .generate_plot(tmp_dat, "var a", "var c", group_var = "var group") # grouped flipped boxplot
+  .generate_plot(tmp_dat, "var quant1", "var quant2") # scatterplot
+  .generate_plot(tmp_dat, "var cat1", "var quant1") # boxplot
+  .generate_plot(tmp_dat, "var quant1", "var cat1") # flipped boxplot
+  .generate_plot(tmp_dat, "var quant1", "var quant2", group_var = "var group") # grouped scatterplot
+  .generate_plot(tmp_dat, "var cat1", "var quant1", group_var = "var group") # grouped boxplot
+  .generate_plot(tmp_dat, "var quant1", "var cat1", group_var = "var group") # grouped flipped boxplot
   expect_true(TRUE)
 })
